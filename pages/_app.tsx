f56481e76ca451236c7157fe09b1f1098b992a3c
fabioNog <fabio.carmo@computacao.ufla.br>
2021-02-06 16:46:03 -0300

    Add Home.module.css globals.css pages/posts sass ant.design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../public/globals.css'
+import './globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-import styles from '../public/Home.module.css'
+import styles from './Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Kitsu App</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
