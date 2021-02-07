ea0ccef4d75182b654f9e3b9bbd892da36086a5a
fabioNog <fabio.carmo@computacao.ufla.br>
2021-02-07 13:19:29 -0300

    Adicionando Menu ant design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import './globals.css'
+import 'antd/dist/antd.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+export default function MyApp({ Component, pageProps }) {
+  
+  return <
+    Component {...pageProps} 
+  />
 }
 
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,39 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
+
 import styles from './Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
+/* Ant Design */
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+
+export default function FirstPost() {
+  const [ current, setCurrent] = useState('mail')
+
+  /* const handleClick = (e) => {
+    setCurrent({ current: e.key })
+  }; */
+  
+  return(
+    <div>
       <Head>
         <title>Kitsu App</title>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.ts!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Menu onClick={(e) =>{ current: e.key }} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="mail" icon={<MailOutlined />}>
+          Home
+        </Menu.Item>
+      
+        <Menu.Item key="alipay">
+          <Link href="/posts/post">Posts</Link>                
+        </Menu.Item>
+      </Menu>
     </div>
-  )
+    
+  ) 
+  
 }
